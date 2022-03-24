a542e5b4ce92d0f724b309dd89b9692c234a8c54
Gopi Rayini <gopi.rayini@gmail.com>
2022-03-24 19:25:30 -0400

    Code clean up

--- main.py
+++ main.py
@@ -42,50 +42,51 @@ def add_name(name):
     namelist.append(name)
     db["name"] = namelist
 
+def checkName(msg):
+    if (msg==('joe' or 'Joe')):
+        return('**Joe** is a fantastic gamer. Just needs to work on communication, aim, map awareness, crosshair placement, economy management, pistol aim, awp flicks, grenade spots, smoke spots, pop flashes, positioning, bomb plant positions, retake ability, bunny hopping, spray control and getting a kill.')
+
+    if (msg==('raven' or 'Raven')):
+        return('**Raven**: I am loli maid with cat ears. Smol, cute, and very smol. Am like 3ft 4in. Anyways, give me headpats')
+
+    if (msg==('gopi' or "Gopi")):
+        return("**Gopi** : I'm the owner of this bot, bitch")
+
+    if (msg==('xinyi' or 'Xinyi')):
+        return("**Xinyi**: 我有最大的阴茎")
+
+    if (msg==('justin' or 'Justin' or 'jj')):
+        return("**JJSLAYIN**: I only play games that require 3000+ hours to get 1 item.")
+
+    if (msg==('ariana' or 'Ariana')):
+        return("**Ariana**: I am Ariana Pequeno.")
+
+    else:
+        return "NA"   
+
 
 @client.event
 async def on_ready():
-    await client.change_presence(activity=discord.Game('with your mom'))
+    await client.change_presence(activity=discord.Game('gently ;)'))
     print('We have logged in as {0.user}'.format(client))
 
 @client.event
 async def on_message(message):
-
     msg=message.content
     prefix = db["prefix"] 
 
     if message.author == client.user:
         return
 
-    if message.content.startswith(prefix+'prefix'):
-        char = msg.split(prefix+'prefix ',1)[1]
-        update_prefix(char)
-        await message.channel.send("Prefix updated to "+char)
+    if message.content.startswith(prefix):
 
-    def checkName(msg):
-        if (msg==('joe' or 'Joe')):
-            return('**Joe** is a fantastic gamer. Just needs to work on communication, aim, map awareness, crosshair placement, economy management, pistol aim, awp flicks, grenade spots, smoke spots, pop flashes, positioning, bomb plant positions, retake ability, bunny hopping, spray control and getting a kill.')
+        #Parse command only without prefix
+        msg = message.content.replace(prefix, '', 1) 
 
-        if (msg==('raven' or 'Raven')):
-            return('**Raven**: I am loli maid with cat ears. Smol, cute, and very smol. Am like 3ft 4in. Anyways, give me headpats')
-
-        if (msg==('gopi' or "Gopi")):
-            return("**Gopi** : I'm the owner of this bot, bitch")
-
-        if (msg==('xinyi' or 'Xinyi')):
-            return("**Xinyi**: 我有最大的阴茎")
-
-        if (msg==('justin' or 'Justin' or 'jj')):
-            return("**JJSLAYIN**: I only play games that require 3000+ hours to get 1 item.")
-
-        if (msg==('ariana' or 'Ariana')):
-            return("**Ariana**: I am Ariana Pequeno.")
-
-        else:
-            return "NA"
-
-    if message.content.startswith(prefix):    
-        msg = message.content.replace(prefix, '', 1) #Parse command only without prefix
+        if(msg=='prefix'):
+            char = msg.split('prefix ', 1)[1]
+            update_prefix(char)
+            await message.channel.send("Prefix updated to "+char)
 
         if (msg=='help'):            
             await message.channel.send("**Commands are:**")
